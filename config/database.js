157ef64c25b268aa131eb196513138f0b1ec159e
Rafael Muttoni <hi@rafaelmuttoni.com>
2021-03-13 15:28:46 -0300

    initialized project + db configs + user and auth

---
+++ config/database.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('@adonisjs/framework/src/Env')} */
+const Env = use("Env");
+
+/** @type {import('@adonisjs/ignitor/src/Helpers')} */
+const Helpers = use("Helpers");
+
+module.exports = {
+  connection: Env.get("DB_CONNECTION", "pg"),
+
+  pg: {
+    client: "pg",
+    connection: {
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", ""),
+      database: Env.get("DB_DATABASE", "adonis"),
+    },
+    debug: Env.get("DB_DEBUG", false),
+  },
+};
